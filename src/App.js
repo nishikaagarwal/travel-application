0eec34ba8ac9846de9adaf915c530d561b2c11ae
Nishika Agarwal <nishika991@gmail.com>
2022-05-29 19:02:05 +0530

    Added filtering and search

--- src/App.js
+++ src/App.js
@@ -9,8 +9,16 @@ import Map from './Components/Map/Map';
 
 function App() {
   const [places, setPlaces] = useState([]);
+  const [childClicked, setChildClicked] = useState(null);
+  const[filteredPlaces, setFilteredPlaces] = useState([]);
+
   const [coordinates, setCoordinates] = useState({ lat:0, lng:0});
   const [bounds, setBounds] = useState({});
+
+  const[isLoading, setIsLoading] = useState(false);
+  const [type, setType] = useState('restaurants');
+  const [rating, setRating] = useState(0);
+
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(({coords: {latitude, longitude}})=>{
@@ -19,23 +27,34 @@ function App() {
   }, [])
 
   useEffect(() => {
-    
-    getPlacesData(bounds.sw, bounds.ne).then((data) => {
-      console.log(data);
-      setPlaces(data)
+    const filteredPlaces = places.filter((place) => place.rating > rating);
+
+    setFilteredPlaces(filteredPlaces);
+  }, [rating]);
+
+  useEffect(() => {
+
+    if (bounds.sw && bounds.ne) {
+    setIsLoading(true);
+
+    getPlacesData(type, bounds.sw, bounds.ne).then((data) => {
+      setPlaces(data?.filter((place) => place.name && place.num_reviews > 0))
+      setIsLoading(false);
+      setFilteredPlaces([]);
     });
-  }, [coordinates,bounds]);
+  }
+  }, [type, coordinates, bounds]);
 
   return (
     <>
       <CssBaseline />
-      <Header />
+      <Header setCoordinates={setCoordinates} />
       <Grid container spacing={3} style={{width: '100%'}}>
         <Grid item xs={12} md={4}>
-          <List places={places}/>
+          <List places={filteredPlaces.length ? filteredPlaces : places} childClicked={childClicked} isLoading={isLoading} type={type} setType={setType} rating={rating} setRating={setRating} />
         </Grid>
         <Grid item xs={12} md={8}>
-          <Map setCoordinates={setCoordinates} setBounds={setBounds} coordinates={coordinates} />
+          <Map setCoordinates={setCoordinates} setBounds={setBounds} coordinates={coordinates} places={filteredPlaces.length ? filteredPlaces : places} setChildClicked={setChildClicked}/>
         </Grid>
       </Grid>
     </>
--- src/Components/List/List.jsx
+++ src/Components/List/List.jsx
@@ -1,19 +1,29 @@
-import React, {useState} from "react";
+import React, {useState, useEffect, createRef} from "react";
 import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from "@material-ui/core";
 import PlaceDetails from '../PlaceDetails/PlaceDetails';
 
 import useStyles from './styles';
 
-const List = ({places}) =>{
+const List = ({places, childClicked, isLoading, type, setType, rating, setRating}) =>{
     const classes = useStyles();
-    const [type, setType] = useState('restaurants');
-    const [rating, setRating] = useState(0);
+   
+    const [elRefs, setElRefs] = useState([]);
 
-  
+    useEffect(() => {
+        const refs = Array(places?.length).fill().map((_, i) => elRefs[i] || createRef());
+
+        setElRefs(refs)
+    }, [places])
 
     return(
         <div className={classes.container}>
             <Typography variant="h4">Resstaurants, Hotels and Attractions</Typography>
+            { isLoading ? (
+                <div className={classes.loading}>
+                    <CircularProgress size="5rem" />
+                </div>
+            ) : (
+            <>
             <FormControl className={classes.formControl}>
                 <InputLabel>Type</InputLabel>
                 <Select value={type} onChange={(e)=> setType(e.target.value)}>
@@ -34,10 +44,12 @@ const List = ({places}) =>{
             <Grid container spacing={3} className={classes.list}>
                 {places?.map((place, i)=> (
                     <Grid item key={i} xs={12}>
-                        <PlaceDetails place={place} />
+                        <PlaceDetails place={place} selected={Number(childClicked )== i} refProp={elRefs[i]} />
                     </Grid>
                 ))}
             </Grid>
+            </>    
+            )}
         </div>
     )
 }
